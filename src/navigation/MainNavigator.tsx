93266022c21fd319526c26fc7a7f77daf61760a2
STchrispin <chrispin237@gmail.com>
2026-06-16 12:15:57 +0100

    navigation

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SoundListScreen from '../screens/audio/SoundListScreen';
+import AdminDashboard from '../screens/admin/AdminDashboard';
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Feed" component={() => null} />
-      <Tab.Screen name="Search" component={() => null} />
-      <Tab.Screen name="Upload" component={() => null} />
-      <Tab.Screen name="Messages" component={() => null} />
-      <Tab.Screen name="Profile" component={() => null} />
+      <Tab.Screen name="Sons" component={SoundListScreen} />
+      <Tab.Screen name="Admin" component={AdminDashboard} />
     </Tab.Navigator>
   );
 };
